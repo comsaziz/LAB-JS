f79d7683772c5c15a3a245a7c4ddba0df140b9ab
comsaziz <comsaziz@gmail.com>
2024-09-09 11:14:51 +0300

    add lab

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+Q1
+for (let i = 0; i <= 25; i++) {
+    if (i % 2 === 0) {  
+      console.log(i);
+    }
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(i * i);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 1) {  
+      console.log(i);
+    }
+  }
+
+
+//   Q2
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + i);
+}
+
+
+
+// Q3
+
+for (let i = 1; i <= 3; g++) { {
+        console.log(i * j);
+    }
+    
+}
+
+
+
+  
+  
